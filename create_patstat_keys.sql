9bdfc229ca97865822a509032c804b0aed6d614f
altay <altay@ikaria>
2019-11-14 14:08:35 +0100

    Changes for PATSTAT 2019 spring edition

--- create_patstat_keys.sql
+++ create_patstat_keys.sql
@@ -1,3 +1,4 @@
+-- defining primary keys
 ALTER TABLE tls201_appln ADD PRIMARY KEY (appln_id);
 
 ALTER TABLE tls202_appln_title ADD PRIMARY KEY (appln_id);
@@ -8,58 +9,73 @@ ALTER TABLE tls204_appln_prior ADD PRIMARY KEY (appln_id, prior_appln_id);
 
 ALTER TABLE tls205_tech_rel ADD PRIMARY KEY (appln_id, tech_rel_appln_id);
 
-ALTER TABLE tls209_appln_ipc ADD PRIMARY KEY (appln_id, ipc_class_symbol, ipc_class_level);
+ALTER TABLE tls206_person ADD PRIMARY KEY (person_id);
+
+ALTER TABLE tls207_pers_appln ADD PRIMARY KEY (person_id, appln_id, applt_seq_nr, invt_seq_nr);
+
+ALTER TABLE tls209_appln_ipc ADD PRIMARY KEY (appln_id, ipc_class_symbol);
 
 ALTER TABLE tls210_appln_n_cls ADD PRIMARY KEY (appln_id, nat_class_symbol);
 
 ALTER TABLE tls211_pat_publn ADD PRIMARY KEY (pat_publn_id);
 
-ALTER TABLE tls212_citation ADD PRIMARY KEY (pat_publn_id, citn_id);
+ALTER TABLE tls212_citation ADD PRIMARY KEY (pat_publn_id, citn_replenished, citn_id);
 
 ALTER TABLE tls214_npl_publn ADD PRIMARY KEY (npl_publn_id);
 
-ALTER TABLE tls215_citn_categ ADD PRIMARY KEY (pat_publn_id, citn_id, citn_categ);
+ALTER TABLE tls215_citn_categ ADD PRIMARY KEY (pat_publn_id, citn_replenished, citn_id, citn_categ, relevant_claim);
 
 ALTER TABLE tls216_appln_contn ADD PRIMARY KEY (appln_id, parent_appln_id);
-
-ALTER TABLE tls218_docdb_fam ADD PRIMARY KEY (appln_id);
-
-ALTER TABLE tls219_inpadoc_fam ADD PRIMARY KEY (appln_id);
 
 ALTER TABLE tls222_appln_jp_class ADD PRIMARY KEY (appln_id, jp_class_scheme, jp_class_symbol);
 
 ALTER TABLE tls223_appln_docus ADD PRIMARY KEY (appln_id, docus_class_symbol);
 
+ALTER TABLE tls224_appln_cpc ADD PRIMARY KEY (appln_id, cpc_class_symbol, cpc_scheme);
+
+ALTER TABLE tls226_person_orig ADD PRIMARY KEY (person_orig_id);
+
 ALTER TABLE tls227_pers_publn ADD PRIMARY KEY (person_id, pat_publn_id, applt_seq_nr, invt_seq_nr);
+
+ALTER TABLE tls228_docdb_fam_citn ADD PRIMARY KEY (docdb_family_id, cited_docdb_family_id);
+
+ALTER TABLE tls229_appln_nace2 ADD PRIMARY KEY (appln_id, nace2_code);
+
+ALTER TABLE tls230_appln_techn_field ADD PRIMARY KEY (appln_id, techn_field_nr);
+
+ALTER TABLE tls231_inpadoc_legal_event ADD PRIMARY KEY (event_id);
 
 ALTER TABLE tls801_country ADD PRIMARY KEY (country_code);
 
-ALTER TABLE tls901_techn_field_ipc ADD PRIMARY KEY (techn_field_nr, ipc_maingroup_symbol);
+ALTER TABLE tls803_legal_event_code ADD PRIMARY KEY (event_auth, event_code);
+
+ALTER TABLE tls901_techn_field_ipc ADD PRIMARY KEY (ipc_maingroup_symbol);
+
+ALTER TABLE tls902_ipc_nace2 ADD PRIMARY KEY (ipc, not_with_ipc, unless_with_ipc, nace2_code);
+
+ALTER TABLE tls904_nuts ADD PRIMARY KEY (nuts);
 
 ALTER TABLE tls906_person ADD PRIMARY KEY (person_id);
 
-CREATE INDEX ON tls201_appln (appln_auth, appln_nr, appln_kind);
+------------------------------------------------
+-- defining alternate keys
+CREATE INDEX ON tls201_appln (appln_auth, appln_nr, appln_kind, receiving_office);
 
-CREATE INDEX ON tls201_appln (appln_filing_date);
+CREATE INDEX ON tls206_person (person_name, person_name_orig_lg, person_address, person_ctry_code);
 
-CREATE INDEX ON tls211_pat_publn (appln_id);
+CREATE INDEX ON tls906_person (person_name, person_name_orig_lg, person_address, person_ctry_code);
 
-CREATE INDEX ON tls212_citation (cited_pat_publn_id);
+CREATE INDEX ON tls211_pat_publn (publn_auth, publn_nr, publn_kind, publn_date);
 
-CREATE INDEX ON tls201_appln (internat_appln_id);
+CREATE INDEX ON tls231_inpadoc_legal_event (appln_id, event_seq_nr);
 
-CREATE INDEX ON tls906_person (person_ctry_code);
-
-CREATE INDEX ON tls211_pat_publn (publn_auth, publn_nr, publn_kind);
-
-CREATE INDEX ON tls211_pat_publn (publn_date);
-
+------------------------------------------------
+-- defining foreign keys
 ALTER TABLE tls202_appln_title ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
 
 ALTER TABLE tls203_appln_abstr ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
 
 ALTER TABLE tls204_appln_prior ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
-
 ALTER TABLE tls204_appln_prior ADD FOREIGN KEY (prior_appln_id) REFERENCES tls201_appln(appln_id);
 
 ALTER TABLE tls205_tech_rel ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
@@ -68,6 +84,8 @@ ALTER TABLE tls205_tech_rel ADD FOREIGN KEY (tech_rel_appln_id) REFERENCES tls20
 
 ALTER TABLE tls207_pers_appln ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
 
+ALTER TABLE tls207_pers_appln ADD FOREIGN KEY (person_id) REFERENCES tls206_person(person_id);
+
 ALTER TABLE tls209_appln_ipc ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
 
 ALTER TABLE tls210_appln_n_cls ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
@@ -75,15 +93,13 @@ ALTER TABLE tls210_appln_n_cls ADD FOREIGN KEY (appln_id) REFERENCES tls201_appl
 ALTER TABLE tls211_pat_publn ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
 
 ALTER TABLE tls212_citation ADD FOREIGN KEY (cited_appln_id) REFERENCES tls201_appln(appln_id);
-
 ALTER TABLE tls212_citation ADD FOREIGN KEY (cited_pat_publn_id) REFERENCES tls211_pat_publn(pat_publn_id);
-
 ALTER TABLE tls212_citation ADD FOREIGN KEY (pat_publn_id) REFERENCES tls211_pat_publn(pat_publn_id);
+ALTER TABLE tls212_citation ADD FOREIGN KEY (cited_npl_publn_id) REFERENCES tls214_npl_publn(npl_publn_id);
 
-ALTER TABLE tls215_citn_categ ADD FOREIGN KEY (pat_publn_id, citn_id) REFERENCES tls212_citation(pat_publn_id, citn_id);
+ALTER TABLE tls215_citn_categ ADD FOREIGN KEY (pat_publn_id, citn_replenished, citn_id) REFERENCES tls212_citation(pat_publn_id, citn_replenished, citn_id);
 
 ALTER TABLE tls216_appln_contn ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
-
 ALTER TABLE tls216_appln_contn ADD FOREIGN KEY (parent_appln_id) REFERENCES tls201_appln(appln_id);
 
 ALTER TABLE tls218_docdb_fam ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
@@ -96,4 +112,24 @@ ALTER TABLE tls223_appln_docus ADD FOREIGN KEY (appln_id) REFERENCES tls201_appl
 
 ALTER TABLE tls224_appln_cpc ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
 
+ALTER TABLE tls227_pers_publn ADD FOREIGN KEY (person_id) REFERENCES tls206_person(person_id);
 ALTER TABLE tls227_pers_publn ADD FOREIGN KEY (pat_publn_id) REFERENCES tls211_pat_publn(pat_publn_id);
+
+ALTER TABLE tls228_docdb_fam_citn ADD FOREIGN KEY (docdb_family_id) REFERENCES tls201_appln(docdb_family_id);
+ALTER TABLE tls228_docdb_fam_citn ADD FOREIGN KEY (cited_docdb_family_id) REFERENCES tls201_appln(docdb_family_id);
+
+ALTER TABLE tls229_appln_nace2 ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
+ALTER TABLE tls229_appln_nace2 ADD FOREIGN KEY (nace2_code) REFERENCES tls902_ipc_nace2(nace2_code);
+
+ALTER TABLE tls230_appln_techn_field ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
+ALTER TABLE tls230_appln_techn_field ADD FOREIGN KEY (techn_field_nr) REFERENCES tls901_techn_field_ipc(techn_field_nr);
+
+ALTER TABLE tls231_inpadoc_legal_event ADD FOREIGN KEY (appln_id) REFERENCES tls201_appln(appln_id);
+ALTER TABLE tls231_inpadoc_legal_event ADD FOREIGN KEY (event_auth, event_code) REFERENCES tls803_legal_event_code(event_auth, event_code);
+
+ALTER TABLE tls801_country ADD FOREIGN KEY (ctry_code) REFERENCES tls206_person(person_ctry_code);
+ALTER TABLE tls801_country ADD FOREIGN KEY (ctry_code) REFERENCES tls906_person(person_ctry_code); 
+
+ALTER TABLE tls901_techn_field_ipc ADD FOREIGN KEY (techn_field_nr) REFERENCES tls209_appln_ipc(tech_field_nr);
+
+ALTER TABLE tls904_nuts ADD FOREIGN KEY (nuts) REFERENCES tls906_person(nuts); 
--- insert_data_to_patstat.sh
+++ insert_data_to_patstat.sh
@@ -19,13 +19,27 @@ if [ ! -e ./create_patstat_keys.sql ]; then
     exit 0
 fi
 
-# creating tables within the PATSTAT database
-## psql patstat < ./create_patstat_tables.sql
+# check if the patstat database is created
+if psql -lqt | cut -d \| -f 1 | grep -qw patstat; then
+    # database exists
+    echo "patstat database exists, installation starts"
+    
+else
+
+    echo "patstat database doesn't exist."
+    echo "to create the PATSTAT DB you should become a postgres user then in the postgres shell enter the following commands;
+postgres=# CREATE USER your_user_name WITH PASSWORD 'your_password';
+postgres=# CREATE DATABASE patstat;
+postgres=# GRANT ALL PRIVILEGES ON DATABASE patstat to your_user_name;"
+    exit 0
+fi
 
 read -e -p "Enter the path to the zip files: [/path/to/patstat/zip_files]" ZIP_FILES_DIR
 
-TMP_DIR=$ZIP_FILES_DIR/tmp
+## defining tmp directory 
+TMP_DIR="$ZIP_FILES_DIR/tmp"
 
+## defining tmp directory 
 if [ ! -d $TMP_DIR ]; then
     mkdir $TMP_DIR
 fi
@@ -33,29 +47,39 @@ fi
 # list of tables to be filled
 table_list="	
 tls201_appln
-tls202_appln_title	
-tls203_appln_abstr	
-tls204_appln_prior	
-tls205_tech_rel		
+tls202_appln_title
+tls203_appln_abstr
+tls204_appln_prior
+tls205_tech_rel
 tls206_person
-tls207_pers_appln	
-tls209_appln_ipc	
+tls207_pers_appln
+tls209_appln_ipc
 tls210_appln_n_cls
-tls211_pat_publn	
+tls211_pat_publn
 tls212_citation
 tls214_npl_publn
-tls215_citn_categ	
-tls216_appln_contn	
-tls222_appln_jp_class	
-tls223_appln_docus	
+tls215_citn_categ
+tls216_appln_contn
+tls222_appln_jp_class
+tls223_appln_docus
 tls224_appln_cpc
+tls226_person_orig
 tls227_pers_publn
 tls228_docdb_fam_citn
 tls229_appln_nace2
 tls230_appln_techn_field
+tls231_inpadoc_legal_event
 tls801_country
+tls803_legal_event_code
 tls901_techn_field_ipc
-tls902_ipc_nace2"
+tls902_ipc_nace2
+tls904_nuts
+tls906_person"
+
+# creating tables within the PATSTAT database
+psql patstat < ./create_patstat_tables.sql
+
+read -e -p "Do you want to create keys, indexes just after the data is inserted? y/n " INDEX_Y
 
 # control if there are zip files in the given directory
 count=`ls $ZIP_FILES_DIR/*.zip 2>/dev/null | wc -l`
@@ -86,5 +110,14 @@ done
 rm $TMP_DIR/file_to_be_inserted.csv
 rmdir $TMP_DIR
 
-# creating index, it will take very long hours, don't despair :)
-## psql patstat < ./create_patstat_keys.sql
+# index creation, it will take very long hours, don't despair :)
+if [ $INDEX_Y == 'y' ]; then
+    echo "creating indexes and keys, it will take very long hours, don't despair :)"
+    psql patstat < ./create_patstat_keys.sql 
+else
+    echo "You choose not to create keys, indexes. Please run\n"
+    echo "$ psql patstat < ./create_patstat_keys.sql"
+fi
+
+
+echo "The script has finished."
