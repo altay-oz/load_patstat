064eaa0fbd7a75f5ebc407c1422cd6164d0c1e7e
Ian van der Linde <ivdl@sun.ac.za>
2022-11-11 14:44:34 +0200

    Revert "Simplify scripts."
    This reverts commit 84f229fa58a4cc36db02a3547a2caddcdcf4afc0.

--- count_rows_in_patstat.sh
+++ count_rows_in_patstat.sh
@@ -2,7 +2,6 @@
 # count row numbers in patstat db
 
 IFS=$'\n'
-export PGPASSWORD=''
 
 # row count for each table documented by EPO in text file located at /CreateScripts/
 
@@ -40,7 +39,7 @@ tls904_nuts"
 for i in ${table_name_count}
 do
     table_name=`echo $i | cut -f1 -d" "`
-    db_count=`psql -U patstat -h localhost -t -c "SELECT count(*) FROM $table_name" patstat`
+    db_count=`psql -t -c "SELECT count(*) FROM $table_name" patstat`
 
     echo "table_name = $table_name, db_count = $db_count"
     echo " "
--- insert_data_to_patstat.sh
+++ insert_data_to_patstat.sh
@@ -4,7 +4,7 @@
 
 # this script has tree parts
 # 1- creating tables in PATSTAT
-# 2- inserting data into the database by using csv files.
+# 2- inserting data into the database by using zip files obtained from CDs.
 # 3- creaing indexes in PATSTAT. This one is taking more time than the
 #    second step.
 
@@ -19,8 +19,30 @@ if [ ! -e ./create_patstat_keys.sql ]; then
     exit 0
 fi
 
-export PGPASSWORD=''
-read -e -p "Enter the path to the csv files: [/path/to/patstat/zip_files] " CSV_FILES_DIR
+# check if the patstat database is created
+if psql -lqt | cut -d \| -f 1 | grep -qw patstat; then
+    # database exists
+    echo "patstat database exists, installation starts"
+
+else
+
+    echo "patstat database doesn't exist."
+    echo "to create the PATSTAT DB you should become a postgres user then in the postgres shell enter the following commands;
+postgres=# CREATE USER your_user_name WITH PASSWORD 'your_password';
+postgres=# CREATE DATABASE patstat;
+postgres=# GRANT ALL PRIVILEGES ON DATABASE patstat to your_user_name;"
+    exit 0
+fi
+
+read -e -p "Enter the path to the zip files: [/path/to/patstat/zip_files] " ZIP_FILES_DIR
+
+## defining tmp directory
+TMP_DIR=$ZIP_FILES_DIR"tmp/"
+
+## defining tmp directory
+if [ ! -d "$TMP_DIR" ]; then
+    mkdir "$TMP_DIR"
+fi
 
 # list of tables to be filled
 table_list="
@@ -55,7 +77,17 @@ tls902_ipc_nace2
 tls904_nuts"
 
 # creating tables within the PATSTAT database
-psql -h localhost -U patstat patstat < ./create_patstat_tables.sql
+psql patstat < ./create_patstat_tables.sql
+
+read -e -p "Do you want to create keys, indexes just after the data is inserted? y/n " INDEX_Y
+
+# control if there are zip files in the given directory
+# counting the number of zip files
+count=`ls "$ZIP_FILES_DIR"*.zip 2>/dev/null | wc -l`
+if [ $count == 0 ]; then
+    echo "There is no zip file in "$ZIP_FILES_DIR
+    exit 0
+fi
 
 for table_name in $table_list
 do
@@ -64,16 +96,30 @@ do
    base_name="${table_name:0:6}"
    echo $base_name
 
-   # for files starting tlsXXX_partXXX.csv
-   for file in "$CSV_FILES_DIR/"$base_name*
+   # for files starting tlsXXX_partXXX.gzip
+   for file in "$ZIP_FILES_DIR"$base_name*
    do
-       psql -h localhost -U patstat -c "\COPY $table_name from '$file' DELIMITER AS ',' CSV HEADER QUOTE AS '\"' " patstat
+       # file tlsXXX_partXXX.zip is unziped in a temporary directory
+       echo "unziping $file"
+       unzip -p "$file" > "$TMP_DIR/"file_to_be_inserted.csv
+       psql -c "\COPY $table_name from '$TMP_DIR/file_to_be_inserted.csv' DELIMITER AS ',' CSV HEADER QUOTE AS '\"' " patstat
        echo "INSERTED $file"
        echo " "
    done
 done
 
-echo "creating indexes and keys, it will take very long hours, don't despair :)"
-psql -h localhost -U patstat patstat < ./create_patstat_keys.sql 
+# cleaning the rest
+rm "$TMP_DIR"file_to_be_inserted.csv
+rmdir "$TMP_DIR"
+
+# index creation, it will take very long hours, don't despair :)
+if [ $INDEX_Y == 'y' ]; then
+    echo "creating indexes and keys, it will take very long hours, don't despair :)"
+    psql patstat < ./create_patstat_keys.sql 
+else
+    echo "You choose not to create keys, indexes. Please run\n"
+    echo "$ psql patstat < ./create_patstat_keys.sql"
+fi
+
 
 echo "The script has finished."
